661222ed22941067f890e4b4437553f0ab9f2098
Chusa007 <chusa007@mail.ru>
2018-12-01 20:08:36 +0300

    FLASK

--- arduino.py
+++ arduino.py
@@ -1,8 +1,8 @@
 import serial
 import time
-ser = serial.Serial('/dev/ttyACM3')
+ser = serial.Serial('/dev/ttyACM0')
 ser.baudrate = 9600
 # ser = serial.Serial('COM3', 9600)
 time.sleep(2)
-ser.write(str.encode("Unknown%HTYGHGTY#"))
+ser.write(str.encode("Unknown#"))
 
